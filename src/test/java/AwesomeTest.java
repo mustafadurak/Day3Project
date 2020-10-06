8cdad0cd106d1ea5eb3d61876c5b48a2592bb25d
mustafadurak <emine.meva.mustafa@gmail.com>
2020-10-06 21:59:30 +0200

    Added new file for awesome feature

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,4 +4,10 @@ public class AwesomeTest {
     //green means it is under staging area
     //grey in . idea folder means it is ignored
     //when the color is normal , it meansit is committed no changes
+
+    //i just pull the new cahange from GitHub that i did not have locally
+    // and now my project is in sync with remote reo
+
+    //now i made more awsome changes and i am about to push this bact to GitHub
+
 }
